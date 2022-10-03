7d9455b6e58b8412da2ba828698ddc322d36ed9e
eutyupin <eu-1982@yandex.ru>
2022-10-03 14:30:40 +0700

    student

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -8,20 +8,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class Main {
+    private static StudentDao studentOperations;
     public static void main(String[] args) {
         EntityManagerFactory emFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
         EntityManager em = emFactory.createEntityManager();
-        StudentDao studentOperations = new StudentDao(em);
+        studentOperations = new StudentDao(em);
 
         for (int i = 0; i < 1000; i++) {
             studentOperations.saveOrUpdate(new Student("Name" + i, "Surname" + i));
         }
-
-        for (Student student : studentOperations.findAll()) {
-            System.out.println(student);
-        }
+        System.out.println(findStudent(25L));
+        studentOperations.deleteById(26L);
+        editStudent(findStudent(25L), "John", "Johnson");
         em.close();
     }
+
+    private static void editStudent(Student student, String newName, String newSurname) {
+        student.setName(newName);
+        student.setSurname(newSurname);
+        studentOperations.saveOrUpdate(student);
+        Student editedStudent = findStudent(student.getId());
+        System.out.println(editedStudent);
+    }
+
+    private static Student findStudent(Long id) {
+        return studentOperations.findById(25L);
+    }
+
 }
