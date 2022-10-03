afa6ffa2dfdb56c4619fa7f1bcd0a154609904c9
eutyupin <eu-1982@yandex.ru>
2022-10-03 14:16:14 +0700

    student

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -1,7 +1,27 @@
 package ru.gb;
 
+import org.hibernate.cfg.Configuration;
+import ru.gb.dao.StudentDao;
+import ru.gb.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        EntityManager em = emFactory.createEntityManager();
+        StudentDao studentOperations = new StudentDao(em);
+
+        for (int i = 0; i < 1000; i++) {
+            studentOperations.saveOrUpdate(new Student("Name" + i, "Surname" + i));
+        }
+
+        for (Student student : studentOperations.findAll()) {
+            System.out.println(student);
+        }
+        em.close();
     }
 }
